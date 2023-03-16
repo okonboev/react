62e931da79b08a35edde36bd35b22bffb785f139
okonboev <baxtiyormahmudovich@gmail.com>
2023-03-16 10:58:31 +0500

    create advanced search

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -2,47 +2,84 @@ import React, {useState} from 'react'
 import { data } from '../mock';
 
 function Map() {
-  const [search, setSearch]=useState("")
-  const [datas, setData]=useState(data)
+  const [search, setSearch] = useState("");
+  const [select, setSelect] = useState("id");
+  const [datas, setData] = useState(data);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
   const onSearch = (event) => {
     setSearch(event.target.value);
-    let res = datas.filter(val=>val.name.includes(search))
+    let res = datas.filter(val=>`${val[select]}`.toLowerCase().includes(search))
     setData(res)
   }
+  const getSelectedVal = (e) =>{
+    setSelect(e.target.value)
+  }
+  console.log(select);
   const onDelete = (id) =>{
     let result = datas.filter(val=>val.id !== id)
     setData(result)
   } 
+  const getValueName = (e) =>{
+    setName(e.target.value)
+  }
+  const getValueAddress = (e) =>{
+    setAddress(e.target.value)
+  }
+
+  const onCreate = () =>{
+    let user = {
+      id:Date.now(),
+      name:name,
+      address:address,
+    }
+    let res = [...datas,user];
+    name && address && setData(res)
+    setName("");
+    setAddress("")
+  }
   return (
     <div>
+      <input value={name} onChange={getValueName} type="text" placeholder='name' />
+      <input value={address} onChange={getValueAddress} type="text" placeholder='address' />
+      <button onClick={onCreate}>add</button>
+      <br />
+      <br />
+      <hr />
       <input type="text" placeholder='search' onChange = {onSearch} />
-      <table border="1">
+      <select onChange={getSelectedVal} name="" id="">
+        <option value="id">id</option>
+        <option value="name">name</option>
+        <option value="address">address</option>
+      </select>
+      <table border="1" width={"70%"}>
         <thead>
           <tr>
             <th>id </th>
-            <th>age</th>
             <th>name</th>
             <th>address</th>
-            <th>email</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {
+            datas.length ?
             datas.map((val,inx)=>{
               return(
                 <tr key = {val.id}>
                   <td>{val.id}</td>
-                  <td>{val.age}</td>
                   <td>{val.name}</td>
                   <td>{val.address}</td>
-                  <td>{val.email}</td>
                   <td>
                     <button onClick={() => onDelete(val.id)}>delete</button>
+                    <button >edit</button>
                   </td>
                 </tr>
               )
-            })
+            }) : 
+            <tr>
+                <th colSpan={"4"}><h1>No Data</h1></th>
+            </tr>
           }
         </tbody>
       </table>
--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 
 let arr = [1,2,2,4,5,6];
-let a=arr.map((val,inx,arrr)=>{
-    return(
-        val+"qiymat"
-    )
-});
-console.log(a);
+let arr2 = [...arr,7]
+console.log(arr);
+console.log(arr2);
