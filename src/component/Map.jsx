1fd3bf8c0c8ea8dcd4c5105288eebf7f469ac968
okonboev <baxtiyormahmudovich@gmail.com>
2023-03-17 11:21:01 +0500

    save edit

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -7,6 +7,9 @@ function Map() {
   const [datas, setData] = useState(data);
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [active, setActive] = useState(null);
+  const [names, setNames] = useState("");
+  const [addresses, setAddresses] = useState("");
   const onSearch = (event) => {
     setSearch(event.target.value);
     let res = datas.filter(val=>`${val[select]}`.toLowerCase().includes(search))
@@ -38,6 +41,27 @@ function Map() {
     setName("");
     setAddress("")
   }
+  const onEdit = (val)=>{
+      setActive(val)
+      setNames(val.name)
+      setAddresses(val.address)
+  }
+  const onSave = ()=>{
+    let res = datas.map((val)=>active.id==val.id ? {...val, name:names,address:addresses} : val )
+    setData(res)
+    console.log(res);
+    setActive(null)
+  }
+
+
+
+  const onChangeName = (e) => {
+    setNames(e.target.value)
+  }
+  const onChangeAddress = (e) => {
+    setAddresses(e.target.value)
+
+  }
   return (
     <div>
       <input value={name} onChange={getValueName} type="text" placeholder='name' />
@@ -68,15 +92,25 @@ function Map() {
               return(
                 <tr key = {val.id}>
                   <td>{val.id}</td>
-                  <td>{val.name}</td>
-                  <td>{val.address}</td>
+                  <td>
+                    {
+                      active?.id === val.id ? <input onChange={onChangeName} value={names} type="text" /> : val.name
+                    }
+                  </td>
+                  <td>
+                    {
+                      active?.id === val.id ? <input onChange={onChangeAddress} value={addresses} type="text" /> : val.address
+                    }
+                  </td>
                   <td>
                     <button onClick={() => onDelete(val.id)}>delete</button>
-                    <button >edit</button>
+                    {
+                      active?.id === val.id ? <button onClick={onSave}>save</button> : <button onClick={()=>onEdit(val)}>edit</button>
+                    }
                   </td>
                 </tr>
               )
-            }) : 
+            }) :
             <tr>
                 <th colSpan={"4"}><h1>No Data</h1></th>
             </tr>
@@ -86,7 +120,6 @@ function Map() {
     </div>
   )
 }
-
 
 export default Map;
 
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 
-let arr = [1,2,2,4,5,6];
-let arr2 = [...arr,7]
-console.log(arr);
-console.log(arr2);
+let obj = {
+    names:"ttttt",
+    title:"rrrrr"
+}
+ 
+let gg= {...obj,names:"jjj"}
+console.log(gg);
