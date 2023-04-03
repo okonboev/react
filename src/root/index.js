b42d38537c445ec22e484807acac21e2c557b89d
okonboev <baxtiyormahmudovich@gmail.com>
2023-04-03 11:50:11 +0500

    local storage

--- src/root/index.js
+++ src/root/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
 import Body from '../component/Body'
+import Local from '../component/Local'
 
 function Root() {
   return (
     <div>
-        <Navbar />
-        <Body />
+        {/* <Navbar />
+        <Body /> */}
+        <Local />
     </div>
   )
 }
